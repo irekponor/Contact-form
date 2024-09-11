5b52d0e298a58706106902cc587b0a86c1df00f1
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-11 03:50:07 +0100

    process

--- process.php
+++ process.php
@@ -1,4 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["fullname"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 }
