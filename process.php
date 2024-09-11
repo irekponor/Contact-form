7e3a2615ef15a0d81921577628b140d7ee0c5768
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-11 03:56:59 +0100

    process

--- process.php
+++ process.php
@@ -4,4 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $subject = $_POST["subject"];
     $message = $_POST["message"];
+    $to = "emmanuelirekponor86@gmail.com";
+    $header = "Form $email";
+    mail($to, $subject, $message, $header);
 }
